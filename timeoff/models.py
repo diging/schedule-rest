12662132f7398a9331f08cab07140905ccb5f3a7
yuvakurakula <ykurakul@asu.edu>
2020-05-07 15:33:01 -0700

    ds-1

--- timeoff/models.py
+++ timeoff/models.py
@@ -6,16 +6,13 @@ from accounts.models import User
 
 # Create your models here.
 class Timeoff(models.Model):
-    id = models.AutoField(primary_key=True)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     requested_timeoff = models.DateField(auto_now=False)
     text_box = models.TextField(max_length=500)
     submission_date = models.DateTimeField('date submitted',default=timezone.now)
 
-
-
 class Status(models.Model):
     STATUS_CHOICES = [ ('A','Approved'),('D','Denied'),('P','Pending')]
-    id = models.OneToOneField('Timeoff', on_delete=models.CASCADE, primary_key=True)
+    timeoff = models.OneToOneField('Timeoff', on_delete=models.CASCADE)
     status = models.CharField('status', choices=STATUS_CHOICES, max_length=1)
     approved_by = models.ForeignKey(User, on_delete=models.CASCADE)
