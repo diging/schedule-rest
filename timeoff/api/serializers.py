e6fc36fd658c50a248e5cfe3ba452eebce43700f
yuvakurakula <ykurakul@asu.edu>
2020-05-07 13:59:32 -0700

    ds2

--- timeoff/api/serializers.py
+++ timeoff/api/serializers.py
@@ -27,28 +27,3 @@ class StatusSerializer(serializers.ModelSerializer):
         instance.status = validated_data.get('status', instance.status)
         instance.save()
         return instance
-
-# class TimeoffSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(label='ID',read_only=True)
-#     username = serializers.CharField(required=True, allow_blank=False)
-#     requested_timeoff = serializers.DateField(required=True)
-#     text_box = serializers.CharField()
-#     created = serializers.DateTimeField()
-      # def create(self, validated_data):
-      #     # Create and return a new Timeoff instance, given the validated data.
-      #     return Timeoff.objects.create(**validated_data)
-#     def create(self, validated_data):
-#         # Create and return a new Timeoff instance, given the validated data.
-#         return Timeoff.objects.create(**validated_data)
-#     def delete(self, id, validated_data):
-#         # To delete an instance based on the primary_key id
-#         instance = Timeoff.objects.get(id=id)
-#         instance.delete()
-#     def return_list(self,User,validated_data):
-#         return Timeoff.objects.all().filter(username=User)
-
-# class StatusSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(label='ID',read_only=True)
-#     username = serializers.CharField(required=True, allow_blank=False)
-#     approved_by = models.ForeignKey(User, on_delete=models.CASCADE)
-#     status = serializers.CharField(max_length=1)
