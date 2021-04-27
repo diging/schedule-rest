3395af4aef836d84936bbea863595ed8cea7f03b
sudheera <sudheerad9@gmail.com>
2021-04-27 11:02:38 -0700

    [task/DS-13] Added patch view for admin users to approve/deny requests

--- timeoff/urls.py
+++ timeoff/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import list_all_timeoff_requests, list_user_timeoff_requests, submit_timeoff_view
+from .views import list_all_timeoff_requests
+from .views import list_user_timeoff_requests
+from .views import submit_timeoff_view
+from .views import review_user_timeoff_request
 
 app_name = 'timeoff'
 
@@ -11,4 +14,5 @@ urlpatterns = [
 	path('user/', list_user_timeoff_requests, name='list_user_timeoff_requests'),
 	path('all/', list_all_timeoff_requests, name='list_all_timeoff_requests'),
 	path('create/', submit_timeoff_view, name='submit_timeoff_view'),
+	path('<int:pk>' + '/review_user_request/', review_user_timeoff_request, name='review_user_timeoff_request')
 ]
--- timeoff/views.py
+++ timeoff/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 
@@ -10,7 +13,6 @@ from .serializers import TimeoffSerializer, TimeoffPostSerializer
 class TimeoffViewSet(viewsets.ModelViewSet):
 	queryset = Timeoff.objects.all()
 	serializer_class = TimeoffSerializer
-
 
 @api_view(['GET'])
 def list_user_timeoff_requests(request):
@@ -38,4 +40,15 @@ def submit_timeoff_view(request):
 			timeoff_type = serializer.validated_data['timeoff_type']
 		)
 		return Response(status=status.HTTP_201_CREATED)
+	return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PATCH'])
+@permission_classes([IsAdminUser])
+def review_user_timeoff_request(request, pk):
+	timeoff_object = get_object_or_404(Timeoff, id=pk)
+	request.data["approved_by"] = request.user.id
+	serializer = TimeoffSerializer(timeoff_object, data=request.data, partial=True)
+	if serializer.is_valid():
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_200_OK)
 	return Response(status=status.HTTP_400_BAD_REQUEST)
