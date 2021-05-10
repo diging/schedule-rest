150d915e6046f99bc9081388d03bb919df1a0008
sudheera <sudheerad9@gmail.com>
2021-05-10 11:41:43 -0700

    [DS-2] Added url for the viewset

--- timeoff/urls.py
+++ timeoff/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
-from .views import list_all_timeoff_requests, list_user_timeoff_requests, submit_timeoff_view, review_user_timeoff_request
+from . import views
+from .views import list_all_timeoff_requests, list_user_timeoff_requests, submit_timeoff_view, review_user_timeoff_request, TimeoffViewSet
 
 app_name = 'timeoff'
 
 router = routers.SimpleRouter()
+router.register(r'timeoff', views.TimeoffViewSet)
 
 urlpatterns = [
+	path('', include(router.urls)),
 	path('user/', list_user_timeoff_requests, name='list_user_timeoff_requests'),
 	path('all/', list_all_timeoff_requests, name='list_all_timeoff_requests'),
 	path('create/', submit_timeoff_view, name='submit_timeoff_view'),
